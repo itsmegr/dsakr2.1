4b3b9c1d2ed8916c906eacacc93bfb7abff8269a
unigovind <unigovind@gmail.com>
2020-10-10 13:54:18 +0530

    NEW

---
+++ binary_tree_funcs.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef struct btnode *btptr;
+struct btnode
+{
+    struct btnode *lchild;
+    char data;
+    struct btnode *rchild;
+    
+};
+void insert(btptr &T, char k)
+{
+    if(T==NULL)
+    {
+        T = new btnode;
+        T->data = k;
+        T->lchild = NULL;
+        T->rchild = NULL;
+    }
+    char ch;
+    cin>>ch;
+    if(ch!='.')
+    insert(T->lchild, ch);
+    cin>>ch;
+    if(ch!='.')
+    insert(T->rchild, ch);
+}
+void printPreOrder(btptr T)
+{
+    if(T!=NULL)
+    {
+        cout<<T->data<<" ";
+        printPreOrder(T->lchild);
+        printPreOrder(T->rchild);
+    }
+}
+void serialization(btptr T)
+{
+    if(T!=NULL)
+    {
+        cout<<T->data<<" ";
+        serialization(T->lchild);
+        serialization(T->rchild);
+    }
+    else
+    cout<<". ";
+}
+int main()
+{
+    btptr T = NULL;
+    char n;
+    cin>>n;
+    insert(T, n);
+    printPreOrder(T);
+    serialization(T);
+}
